b128d2341412441e963ebb3766e3394b0a90acce
Ubuntu <azureuser@myVm.uifwkvpwqkledahocosaxovmtb.bx.internal.cloudapp.net>
2023-12-01 05:38:51 +0000

    Download files into pvc

--- services/download/download.sh
+++ services/download/download.sh
@@ -1,31 +1,17 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
+# set -Eeuo pipefail
 
-# TODO: maybe just use the .gitignore file to create all of these
-mkdir -vp /data/.cache \
-  /data/embeddings \
-  /data/config/ \
-  /data/models/ \
-  /data/models/Stable-diffusion \
-  /data/models/GFPGAN \
-  /data/models/RealESRGAN \
-  /data/models/LDSR \
-  /data/models/VAE
+mkdir -vp /app/data/.cache \
+  /app/data/embeddings \
+  /app/data/config/ \
+  /app/data/models/ \
+  /app/data/models/Stable-diffusion \
+  /app/data/models/GFPGAN \
+  /app/data/models/RealESRGAN \
+  /app/data/models/LDSR \
+  /app/data/models/VAE
 
 echo "Downloading, this might take a while..."
 
-aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data/models --continue
-
-echo "Checking SHAs..."
-
-parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
-
-cat <<EOF
-By using this software, you agree to the following licenses:
-https://github.com/AbdBarho/stable-diffusion-webui-docker/blob/master/LICENSE
-https://github.com/CompVis/stable-diffusion/blob/main/LICENSE
-https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/LICENSE.txt
-https://github.com/invoke-ai/InvokeAI/blob/main/LICENSE
-And licenses of all UIs, third party libraries, and extensions.
-EOF
+aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /app/data/models --continue
