bfa20bee413418202fac2cc86fc83aa5efdc8c8b
Ubuntu <azureuser@myVm.uifwkvpwqkledahocosaxovmtb.bx.internal.cloudapp.net>
2023-12-11 16:31:34 +0000

    Download service, download to pvc

--- services/download/download.sh
+++ services/download/download.sh
@@ -10,8 +10,10 @@ mkdir -vp /app/data/.cache \
   /app/data/models/GFPGAN \
   /app/data/models/RealESRGAN \
   /app/data/models/LDSR \
-  /app/data/models/VAE
+  /app/data/models/VAE \
+  /app/data/models/sams \
+  /app/data/models/grounding-dino
 
 echo "Downloading, this might take a while..."
 
-aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /app/data/models --continue
+aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /app/data --continue
